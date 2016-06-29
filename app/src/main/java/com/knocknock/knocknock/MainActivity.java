e7e2b60395e383cf4a0cb50d2ba8bb9ea9848f20
ruiqi.li <ruiqi.li@alibaba-inc.com>
2016-06-29 11:40:35 +0800

    INITIAL COMMIT

---
+++ app/src/main/java/com/knocknock/knocknock/MainActivity.java
@@ -0,0 +1,30 @@
+package com.knocknock.knocknock;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webview = (WebView) findViewById(R.id.webview);
+
+        WebSettings settings = webview.getSettings();
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+        settings.setDomStorageEnabled(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setJavaScriptEnabled(true);
+
+        webview.loadUrl("http://granny.io:3000");
+    }
+}
